ef6efe046ffe7c4561be7e15a8651fbb2b84c878
Robin Lemmers <38210@ma-web.nl>
2023-11-21 15:07:31 +0100

    02

--- 01A/app.js
+++ 01A/app.js
@@ -3,7 +3,21 @@ class App
     runApplication()
     {
         console.log("hello world!");
+        let appNaam = "app"
+        let versienummer = 1.0
+        let versiedatum = new Date("2023-11-21")
+        let autheur = "Robin"
+        let copyright = "niet stelen"
+        let distributeur = "-"
+        let darkmode = false
 
+        console.log(appNaam)
+        console.log(versienummer)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
     }
 }
 
