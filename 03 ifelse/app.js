08a7ea2b34157a620b50fdb42f4757ccd7e715ee
Robin Lemmers <38210@ma-web.nl>
2023-11-30 14:56:17 +0100

    01 start

---
+++ 03 ifelse/app.js
@@ -0,0 +1,12 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world")
+        //let id = document.getElementById("newstitle")
+        //let class = document.getElementsByClassName("gamenews")
+    }
+}
+
+let app = new App()
+app.runApplication()
