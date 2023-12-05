3b9d3f3ad8feddd906ccba10aec6cc5fe3fe9549
Robin Lemmers <38210@ma-web.nl>
2023-12-05 14:22:29 +0100

    03 ifelse

--- 03 ifelse/app.js
+++ 03 ifelse/app.js
@@ -3,8 +3,37 @@ class App
     runApplication()
     {
         console.log("hello world")
-        //let id = document.getElementById("newstitle")
-        //let class = document.getElementsByClassName("gamenews")
+        let title = document.getElementById("newstitle")
+        let newsitem1 = document.getElementsByClassName("gamenews")[0]
+        let newsitem2 = document.getElementsByClassName("gamenews")[1]
+        console.log(title)
+
+        let random = Math.random()
+        console.log(random)
+        if (random < 0.2)
+        {
+            title.style.backgroundColor = "rgb(255, 0, 0)";
+            newsitem1.style.backgroundColor = "rgb(255, 0, 168)"
+            newsitem2.style.backgroundColor = "rgb(124, 128, 163)"
+        }
+        else if(random >= 0.2 && random <= 0.6)
+        {
+            title.style.backgroundColor = "rgb(0, 255, 0)"
+            newsitem1.style.backgroundColor = "rgb(168, 168, 0)"
+            newsitem2.style.backgroundColor = "rgb(255, 200, 120)"
+        }
+        else if(random > 0.6 && random <= 0.75)
+        {
+            title.style.backgroundColor = "rgb(0, 0, 255)"
+            newsitem1.style.backgroundColor = "rgb(0, 168, 255)"
+            newsitem2.style.backgroundColor = "rgb(0, 0, 0)"
+        }
+        else
+        {
+            title.style.backgroundColor = "rgb(255, 255, 0"
+            newsitem1.style.backgroundColor = "rgb(255, 168, 255)"
+            newsitem2.style.backgroundColor = "rgb(64, 168, 255)"
+        }
     }
 }
 
