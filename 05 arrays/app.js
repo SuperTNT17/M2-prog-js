754d0722428dae94cabd962e4c5690a7ef850a65
Robin Lemmers <38210@ma-web.nl>
2024-01-16 14:51:18 +0100

    05 array 03 html elementen

--- 05 arrays/app.js
+++ 05 arrays/app.js
@@ -2,14 +2,31 @@ class App
 {
     runApplication()
     {
+        
+        let headers = document.getElementsByTagName("h1")
+        this.LogArray(headers)
+
+        let headersByCssClass = document.getElementsByClassName("bandName")
+        this.LogArray(headersByCssClass)
+        for (let i = 0; i < headersByCssClass.length; i++) {
+            console.log(headersByCssClass[i].innerHTML)
+            headersByCssClass[i].innerHTML = "huh?"
+        }
+
+        headersByCssClass[0].innerHTML = "nieuws"
+        headersByCssClass[1].innerHTML = "reviews"
+        headersByCssClass[2].innerHTML = "commentaar"
+        headersByCssClass[3].innerHTML = "beste forum posts"
+        headersByCssClass[4].innerHTML = "pricewatch"
+
         console.log("hello world!");
         let artists = ["Sabaton", "Pascal Michael Stiefel", "Amaranthe", "Beast in Black", "Powerwolf"]
         console.log(artists)
-        this.LogArray(artists)
+        this.LogArrayWithNumbers(artists)
 
         artists.push("bob dylan")
         artists.push("prince")
-        this.LogArray(artists)
+        this.LogArrayWithNumbers(artists)
 
         let indexToRemove = artists.indexOf("bob dylan")
         artists.splice(indexToRemove, 1)
@@ -18,13 +35,19 @@ class App
 
         artists.push("XI")
         artists.push("Babymetal")
-        this.LogArray(artists)
+        this.LogArrayWithNumbers(artists)
 
         let nummers = [2, 5, 7]
-        this.LogArray(nummers)
+        this.LogArrayWithNumbers(nummers)
     }
 
     LogArray(array){
+        for (let i = 0; i < array.length; i++) {
+            console.log(array[i])
+        }
+    }
+
+    LogArrayWithNumbers(array){
         for (let i = 0; i < array.length; i++) {
             console.log(`${i + 1}: ${array[i]}`)            
         }
