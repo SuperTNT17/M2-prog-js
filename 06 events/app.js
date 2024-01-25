032f44baf6203cabcfa6f756e052dbee8b6eb5eb
Robin Lemmers <38210@ma-web.nl>
2024-01-25 14:32:07 +0100

    06 events 01 events

--- 06 events/app.js
+++ 06 events/app.js
@@ -3,7 +3,11 @@ class App
     runApplication()
     {
         console.log("hello world!");
-        
+        let uiButton = document.getElementById("myButton")
+        let localeFunction = function (e){
+            console.log("click!")
+        }
+        uiButton.addEventListener("click",localeFunction)
     }
 }
 
