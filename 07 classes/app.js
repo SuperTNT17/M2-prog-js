60350d88ca8384b68262db9ef6ddff9d75c6b400
Robin Lemmers <38210@ma-web.nl>
2024-01-30 15:23:23 +0100

    07 classes 02 functions

--- 07 classes/app.js
+++ 07 classes/app.js
@@ -1,29 +1,42 @@
-class App
-{
-    runApplication()
-    {
+class App {
+    runApplication() {
         console.log("hello world!");
-        
+
     }
 }
 
-class Greet{
-    constructor(){
+class Greet {
+    constructor() {
+    }
+
+    showGreeting() {
         console.log("Greetings!")
     }
 }
 
-class GoodBye{
-    constructor(){
+class GoodBye {
+    constructor() {
+    }
+
+    showBye() {
         console.log("Bye Bye")
     }
 }
 
 let greet = new Greet()
-greet = new Greet()
-greet = new Greet()
-greet = new Greet()
+greet.showGreeting()
+
 let bye = new GoodBye()
+bye.showBye()
+
+greet.showGreeting()
+bye.showBye()
+
+greet.showGreeting()
+bye.showBye()
+
+greet.showGreeting()
+bye.showBye()
 
 let app = new App();
 app.runApplication();
